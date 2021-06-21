ed1fd9ce98e261b55660525199f273df4b107c52
dafawlp <86101071+dafawlp@users.noreply.github.com>
2021-06-21 19:11:45 +0700

    latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,13 @@
+<?php
+$angka = 2819129;
+$split = str_split($angka);
+$len = count($split);
+for ($i=0; $i <= $len - 1; $i++) 
+{
+	echo $split[$i];
+	for ($j = 0; $j < $len - $i - 1; $j++) {
+		echo '0';
+	}
+	echo '<br/>'; 
+}
+?>
